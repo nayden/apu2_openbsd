f939eb56c913d9e44500e8389ac2e9906fbc31cf
gonzalo <gonzalo@x61.com.ar>
2017-03-10 22:44:08 -0300

    Add apu2.sh if you don't want compXX.tgz on your Apu2, fix some typos

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # OpenBSD on Apu2
 
 The idea of this repo is to setup an [APU2](http://pcengines.ch/apu2c4.htm) with [OpenBSD](http://openbsd.org) as OS, for
-a FW/GW/AP box at home (or maybe a company).
+a Firewall/Gateway/AP box at home (or maybe a company).
 
-## Usage
+## Makefile usage
 
-Assuming that you already have OpenBSD installed on your APU2 with just an internet connection, you can do this:
+Assuming that you already have OpenBSD installed (with compXX.tgz) on your APU2 with just an internet connection, you can do this:
 
 ```
 # cd /tmp
@@ -19,3 +19,20 @@ Assuming that you already have OpenBSD installed on your APU2 with just an inter
 [+] All done, you can reboot now.
 # reboot 
 ```
+
+## apu2.sh usage
+
+Assuming that you already have OpenBSD installed on your APU2 with just an internet connection, you can do this:
+
+```
+# cd /tmp
+# ftp -o - https://raw.githubusercontent.com/gonzalo-/apu2_openbsd/master/apu2.sh | sh -
+..................
+..................
+... some stuff ...
+..................
+..................
+[+] All done, you can reboot now.
+# reboot
+```
+
---
+++ apu2.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+#
+#
+
+## Debug
+#set -x
+
+echo ""
+echo "[+] Downloading files from https://github.com/gonzalo-/apu2_openbsd"
+echo ""
+cd /tmp
+ftp -V https://raw.githubusercontent.com/gonzalo-/apu2_openbsd/master/boot.conf
+ftp -V https://raw.githubusercontent.com/gonzalo-/apu2_openbsd/master/hostname.{em0,em1,em2,bridge0,vether0}
+ftp -V https://raw.githubusercontent.com/gonzalo-/apu2_openbsd/master/dhcpd.conf
+ftp -V https://raw.githubusercontent.com/gonzalo-/apu2_openbsd/master/dhclient.conf
+ftp -V https://raw.githubusercontent.com/gonzalo-/apu2_openbsd/master/pf.conf
+ftp -V https://raw.githubusercontent.com/gonzalo-/apu2_openbsd/master/relayd.conf
+ftp -V https://raw.githubusercontent.com/gonzalo-/apu2_openbsd/master/sysctl.conf
+echo ""
+echo "[+] Installing files..."
+echo ""
+install -m 0640 -g wheel -o root /tmp/hostname.{em0,em1,em2,bridge0,vether0} /etc
+install -m 0600 -g wheel -o root /tmp/{pf,relayd}.conf /etc
+install -m 0644 -g wheel -o root /tmp/sysctl.conf /etc
+install -m 0644 -g wheel -o root /tmp/dhcpd.conf /etc
+install -m 0644 -g wheel -o root /tmp/dhclient.conf /etc
+install -m 0644 -g wheel -o root /tmp/boot.conf /
+rm /tmp/hostname.{em0,em1,em2,bridge0,vether0} /tmp/boot.conf
+rm /tmp/{pf,relayd}.conf /tmp/sysctl.conf /tmp/dhcpd.conf /tmp/dhclient.conf
+echo ""
+echo "[+] All done, you can reboot now."
+echo ""
