206f944608e20a6da9077a1441894ceb4f807eb7
gonzalo <gonzalo@r0nin.x61.com.ar>
2017-03-15 11:18:57 -0300

    hook httpd.conf on apu2.sh

--- apu2.sh
+++ apu2.sh
@@ -14,19 +14,20 @@ ftp -V https://raw.githubusercontent.com/gonzalo-/apu2_openbsd/master/hostname.{
 ftp -V https://raw.githubusercontent.com/gonzalo-/apu2_openbsd/master/dhcpd.conf
 ftp -V https://raw.githubusercontent.com/gonzalo-/apu2_openbsd/master/dhclient.conf
 ftp -V https://raw.githubusercontent.com/gonzalo-/apu2_openbsd/master/pf.conf
+ftp -V https://raw.githubusercontent.com/gonzalo-/apu2_openbsd/master/httpd.conf
 ftp -V https://raw.githubusercontent.com/gonzalo-/apu2_openbsd/master/relayd.conf
 ftp -V https://raw.githubusercontent.com/gonzalo-/apu2_openbsd/master/sysctl.conf
 echo ""
 echo "[+] Installing files..."
 echo ""
 install -m 0640 -g wheel -o root /tmp/hostname.{em0,em1,em2,bridge0,vether0} /etc
-install -m 0600 -g wheel -o root /tmp/{pf,relayd}.conf /etc
+install -m 0600 -g wheel -o root /tmp/{pf,relayd,httpd}.conf /etc
 install -m 0644 -g wheel -o root /tmp/sysctl.conf /etc
 install -m 0644 -g wheel -o root /tmp/dhcpd.conf /etc
 install -m 0644 -g wheel -o root /tmp/dhclient.conf /etc
 install -m 0644 -g wheel -o root /tmp/boot.conf /
 rm /tmp/hostname.{em0,em1,em2,bridge0,vether0} /tmp/boot.conf
-rm /tmp/{pf,relayd}.conf /tmp/sysctl.conf /tmp/dhcpd.conf /tmp/dhclient.conf
+rm /tmp/{pf,relayd,httpd}.conf /tmp/sysctl.conf /tmp/dhcpd.conf /tmp/dhclient.conf
 echo ""
 echo "[+] All done, you can reboot now."
 echo ""
